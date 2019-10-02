81b2f98a4fc8ee838de40b298fa7ee5c9a89b57b
Juarisco <juarisco@outlook.com>
2019-10-02 17:01:50 -0500

    8.-Probando-reglas-de-validacion

--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -30,7 +30,7 @@ class CreateStatusTest extends TestCase
         $this->actingAs($user);
 
         // 2. When => Cuando hace un post request a status
-        $response = $this->post(route('statuses.store'), ['body' => 'Mi primer status']);
+        $response = $this->postJson(route('statuses.store'), ['body' => 'Mi primer status']);
 
         $response->assertJson([
             'body' => 'Mi primer status'
@@ -40,6 +40,34 @@ class CreateStatusTest extends TestCase
         $this->assertDatabaseHas('statuses', [
             'user_id' => $user->id,
             'body' => 'Mi primer status'
+        ]);
+    }
+
+    function test_a_status_requires_a_body()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => '']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+
+    function test_a_status_requires_a_minimum_length()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => 'asdf']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
         ]);
     }
 }
