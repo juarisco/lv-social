a7624b6df8170e97b0a89c2a963e48f71cc6ad13
Juarisco <juarisco@outlook.com>
2019-10-02 16:06:50 -0500

    6.-URL-para-crear-estados

--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -4,23 +4,28 @@ namespace Tests\Feature;
 
 use App\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateStatusTest extends TestCase
 {
-    use DatabaseMigrations;
+    use RefreshDatabase;
 
     function test_an_authenticated_user_can_create_statuses()
     {
+        // Para evitar que laravel maneje las excepciones
+        $this->withoutExceptionHandling();
+
         // 1. Given => Teniendo un usuario autenticado
         $user = factory(User::class)->create();
         $this->actingAs($user);
 
         // 2. When => Cuando hace un post request a status
-        $this->post(route('status.store'), ['body' => 'Mi primer status']);
+        $this->post(route('statuses.store'), ['body' => 'Mi primer status']);
 
         // 3. Then => Entonces veo un nuevo estado en la base de datos
         $this->assertDatabaseHas('statuses', [
+            'user_id' => $user->id,
             'body' => 'Mi primer status'
         ]);
     }
