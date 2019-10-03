65dd53ab59e9ef4c35ceb4af1a50a2c348dad1ca
Juarisco <juarisco@outlook.com>
2019-10-03 15:43:10 -0500

    10.-Disenando-la-plantilla-con-Bootstrap-4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
-<form action="{{ route('statuses.store') }}" method="post">
-    @csrf
-    <textarea name="body" cols="30" rows="10"></textarea>
-    <button id="create-status">Publicar estado</button>
-</form>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-8 mx-auto">
+            <div class="card border-0 bg-light">
+                <form action="{{ route('statuses.store') }}" method="post">
+                    @csrf
+                    <div class="card-body">
+                        <textarea class="form-control border-0 bg-light" name="body" placeholder="¿Qué estás pensando?"></textarea>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-primary" id="create-status">Publicar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection 
