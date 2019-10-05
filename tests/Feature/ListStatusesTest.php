16ed9589e5f2317775b0ec932d65903917316133
Juarisco <juarisco@outlook.com>
2019-10-05 11:52:59 -0500

    14.-Probando-listado-de-estados

---
+++ tests/Feature/ListStatusesTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Status;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ListStatusesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    function test_can_get_all_statuses()
+    {
+        $this->withoutExceptionHandling();
+
+        $status1 = factory(Status::class)->create(['created_at' => now()->subDays(4)]);
+        $status2 = factory(Status::class)->create(['created_at' => now()->subDays(3)]);
+        $status3 = factory(Status::class)->create(['created_at' => now()->subDays(2)]);
+        $status4 = factory(Status::class)->create(['created_at' => now()->subDays(1)]);
+
+        $response = $this->getJson(route('statuses.index'));
+
+        $response->assertJson([
+            'total' => 4
+        ]);
+
+        $response->assertJsonStructure([
+            'data', 'total', 'first_page_url', 'last_page_url'
+        ]);
+
+        // dd($response->json('data.0.id'));
+        // dd($response->json('data'));
+        $this->assertEquals(
+            $status4->id,
+            $response->json('data.0.id')
+        );
+    }
+}
