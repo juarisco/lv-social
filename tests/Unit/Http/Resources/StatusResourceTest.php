390bca2d4f0f8965a6497e7c2b1888a480ec457e
Juarisco <juarisco@outlook.com>
2019-10-05 17:26:37 -0500

    17.-Testeando-Recursos-API

---
+++ tests/Unit/Http/Resources/StatusResourceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Http\Resources;
+
+use Tests\TestCase;
+use App\Models\Status;
+use App\Http\Resources\StatusResource;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class StatusResourceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    function test_a_status_resources_must_have_the_necessary_fields()
+    {
+        $status = factory(Status::class)->create();
+
+        $statusResource = StatusResource::make($status)->resolve();
+        // dd($statusResource);
+
+        $this->assertEquals($status->body, $statusResource['body']);
+        $this->assertEquals($status->user->name, $statusResource['user_name']);
+        $this->assertEquals('https://aprendible.com/images/default-avatar.jpg', $statusResource['user_avatar']);
+        $this->assertEquals($status->created_at->diffForHumans(), $statusResource['ago']);
+    }
+}
