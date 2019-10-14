3e53bb0fed63528e0b8b4b585eb7bffb353ee883
Juarisco <juarisco@outlook.com>
2019-10-14 11:59:04 -0500

    45.-Modificando-el-registro-de-usuario

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    function test_users_can_register()
+    {
+        // $this->withoutExceptionHandling();
+
+        $userData = [
+            'name' => 'JosefWaelchi',
+            'first_name' => 'Josef',
+            'last_name' => 'Waelchi',
+            'email' => 'josef@mail.com',
+            'password' => 'secret', // secret
+            'password_confirmation' => 'secret',
+        ];
+
+        $response = $this->post(route('register'), $userData);
+
+        $response->assertRedirect('/');
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'JosefWaelchi',
+            'first_name' => 'Josef',
+            'last_name' => 'Waelchi',
+            'email' => 'josef@mail.com',
+        ]);
+
+        $this->assertTrue(
+            Hash::check('secret', User::first()->password),
+            'The passwor needs to be hashed'
+        );
+    }
+}
