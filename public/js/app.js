63e16216c942d487dda25d937b43129bcd54893d
Juarisco <juarisco@outlook.com>
2019-10-20 16:22:51 -0500

    74.-Likes-en-tiempo-real-parte-4

--- public/js/app.js
+++ public/js/app.js
@@ -59989,7 +59989,7 @@ exports = module.exports = __webpack_require__(1)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -60000,37 +60000,8 @@ exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__LikeBtn__ = __webpack_require__(14);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__LikeBtn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__LikeBtn__);
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__CommentListItem__ = __webpack_require__(94);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__CommentListItem___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__CommentListItem__);
 //
 //
 //
@@ -60046,7 +60017,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-  components: { LikeBtn: __WEBPACK_IMPORTED_MODULE_0__LikeBtn___default.a },
+  components: { CommentListItem: __WEBPACK_IMPORTED_MODULE_0__CommentListItem___default.a },
   props: {
     comments: {
       type: Array,
@@ -60083,63 +60054,13 @@ var render = function() {
   return _c(
     "div",
     _vm._l(_vm.comments, function(comment) {
-      return _c("div", { key: comment.id, staticClass: "mb-3" }, [
-        _c("div", { staticClass: "d-flex" }, [
-          _c("img", {
-            staticClass: "rounded shadow-sm mr-2",
-            attrs: {
-              height: "34px",
-              width: "34px",
-              src: comment.user.avatar,
-              alt: comment.user.name
-            }
-          }),
-          _vm._v(" "),
-          _c(
-            "div",
-            { staticClass: "flex-grow-1" },
-            [
-              _c("div", { staticClass: "card border-0 shadow-sm" }, [
-                _c("div", { staticClass: "card-body p-2 text-secondary" }, [
-                  _c("a", { attrs: { href: comment.user.link } }, [
-                    _c("strong", [_vm._v(_vm._s(comment.user.name))])
-                  ]),
-                  _vm._v(
-                    "\n            " + _vm._s(comment.body) + "\n          "
-                  )
-                ])
-              ]),
-              _vm._v(" "),
-              _c(
-                "small",
-                {
-                  staticClass:
-                    "badge badge-pill badge-primary py-1 px-2 mt-1 float-right",
-                  attrs: { dusk: "comment-likes-count" }
-                },
-                [
-                  _c("i", { staticClass: "fa fa-thumbs-up" }),
-                  _vm._v(
-                    "\n          " + _vm._s(comment.likes_count) + "\n        "
-                  )
-                ]
-              ),
-              _vm._v(" "),
-              _c("like-btn", {
-                staticClass: "comments-like-btn",
-                attrs: {
-                  dusk: "comment-like-btn",
-                  url: "/comments/" + comment.id + "/likes",
-                  model: comment
-                }
-              })
-            ],
-            1
-          )
-        ])
-      ])
+      return _c("comment-list-item", {
+        key: comment.id,
+        staticClass: "mb-3",
+        attrs: { comment: comment }
+      })
     }),
-    0
+    1
   )
 }
 var staticRenderFns = []
@@ -60979,6 +60900,236 @@ module.exports = {
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 91 */,
+/* 92 */,
+/* 93 */,
+/* 94 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var disposed = false
+function injectStyle (ssrContext) {
+  if (disposed) return
+  __webpack_require__(95)
+}
+var normalizeComponent = __webpack_require__(3)
+/* script */
+var __vue_script__ = __webpack_require__(97)
+/* template */
+var __vue_template__ = __webpack_require__(98)
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = injectStyle
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __vue_template__,
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+Component.options.__file = "resources/js/components/CommentListItem.vue"
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-084dee96", Component.options)
+  } else {
+    hotAPI.reload("data-v-084dee96", Component.options)
+  }
+  module.hot.dispose(function (data) {
+    disposed = true
+  })
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+/* 95 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(96);
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add the styles to the DOM
+var update = __webpack_require__(2)("839eea3a", content, false, {});
+// Hot Module Replacement
+if(false) {
+ // When the styles change, update the <style> tags
+ if(!content.locals) {
+   module.hot.accept("!!../../../node_modules/css-loader/index.js!../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-084dee96\",\"scoped\":false,\"hasInlineConfig\":true}!../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./CommentListItem.vue", function() {
+     var newContent = require("!!../../../node_modules/css-loader/index.js!../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-084dee96\",\"scoped\":false,\"hasInlineConfig\":true}!../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./CommentListItem.vue");
+     if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+     update(newContent);
+   });
+ }
+ // When the module is disposed, remove the <style> tags
+ module.hot.dispose(function() { update(); });
+}
+
+/***/ }),
+/* 96 */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(1)(false);
+// imports
+
+
+// module
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+
+// exports
+
+
+/***/ }),
+/* 97 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__LikeBtn__ = __webpack_require__(14);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__LikeBtn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__LikeBtn__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  components: { LikeBtn: __WEBPACK_IMPORTED_MODULE_0__LikeBtn___default.a },
+  props: {
+    comment: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted: function mounted() {
+    var _this = this;
+
+    Echo.channel("comments." + this.comment.id + ".likes").listen("ModelLiked", function (comment) {
+      _this.comment.likes_count++;
+    });
+
+    Echo.channel("comments." + this.comment.id + ".likes").listen("ModelUnLiked", function (comment) {
+      _this.comment.likes_count--;
+    });
+  }
+});
+
+/***/ }),
+/* 98 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", { staticClass: "d-flex" }, [
+    _c("img", {
+      staticClass: "rounded shadow-sm mr-2",
+      attrs: {
+        height: "34px",
+        width: "34px",
+        src: _vm.comment.user.avatar,
+        alt: _vm.comment.user.name
+      }
+    }),
+    _vm._v(" "),
+    _c(
+      "div",
+      { staticClass: "flex-grow-1" },
+      [
+        _c("div", { staticClass: "card border-0 shadow-sm" }, [
+          _c("div", { staticClass: "card-body p-2 text-secondary" }, [
+            _c("a", { attrs: { href: _vm.comment.user.link } }, [
+              _c("strong", [_vm._v(_vm._s(_vm.comment.user.name))])
+            ]),
+            _vm._v("\n        " + _vm._s(_vm.comment.body) + "\n      ")
+          ])
+        ]),
+        _vm._v(" "),
+        _c(
+          "small",
+          {
+            staticClass:
+              "badge badge-pill badge-primary py-1 px-2 mt-1 float-right",
+            attrs: { dusk: "comment-likes-count" }
+          },
+          [
+            _c("i", { staticClass: "fa fa-thumbs-up" }),
+            _vm._v("\n      " + _vm._s(_vm.comment.likes_count) + "\n    ")
+          ]
+        ),
+        _vm._v(" "),
+        _c("like-btn", {
+          staticClass: "comments-like-btn",
+          attrs: {
+            dusk: "comment-like-btn",
+            url: "/comments/" + _vm.comment.id + "/likes",
+            model: _vm.comment
+          }
+        })
+      ],
+      1
+    )
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+module.exports = { render: render, staticRenderFns: staticRenderFns }
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+    require("vue-hot-reload-api")      .rerender("data-v-084dee96", module.exports)
+  }
+}
 
 /***/ })
 /******/ ]);
